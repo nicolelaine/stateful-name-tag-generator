343038429714e70f0839d27f548b16ef3446bd1b
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2023-10-23 23:34:38 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import NameTag from "./NameTag.js";
+
+import "./styles.css";
+
+const renderNameTag = (name) => <NameTag name={name} key={name} />;
+
+class App extends Component {
+  state = {
+    names: [
+      "Ben",
+      "Poppy",
+      "Smitherson",
+      "Bobcat",
+      "Tyrell",
+      "Scooby",
+      "Abrahim"
+    ]
+  };
+
+  render() {
+    const NameTagElements = this.state.names.map(renderNameTag);
+
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        {NameTagElements}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
